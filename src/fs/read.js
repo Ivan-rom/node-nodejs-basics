2db7b9616f9c4e3771411101158c20538c3636cc
Ivan-rom <rom_1van@icloud.com>
2024-10-01 23:36:10 +0600

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  const path = "./src/fs/files";
+  const file = `${path}/fileToRead.txt`;
+
+  await fs.access(file, fs.constants.F_OK, (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
+
+  await fs.readFile(file, "utf8", (err, data) => {
+    if (err) console.log(err);
+
+    console.log(data);
+  });
 };
 
 await read();
