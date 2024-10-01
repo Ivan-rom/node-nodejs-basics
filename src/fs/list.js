a6649a79450995bef5760b0f50c09199d288f01a
Ivan-rom <rom_1van@icloud.com>
2024-10-01 23:29:58 +0600

    feat: implement fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+  const path = "./src/fs/files";
+
+  await fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) throw new Error("FS operation failed");
+
+    fs.readdir(path, { recursive: true }, (error, files) => {
+      if (error) console.log(error);
+
+      files.forEach((file) => {
+        console.log(file);
+      });
+    });
+  });
 };
 
 await list();
