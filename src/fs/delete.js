949b1458e72404f5e48aea0419f3ac3ea1532d6d
Ivan-rom <rom_1van@icloud.com>
2024-10-01 23:04:40 +0600

    feat: implement fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+  const path = "./src/fs/files";
+  const file = `${path}/fileToRemove.txt`;
+
+  await fs.access(file, fs.constants.F_OK, (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
+
+  await fs.unlink(file, (error) => {
+    if (error) console.log(error);
+  });
 };
 
 await remove();
