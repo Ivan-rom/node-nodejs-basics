3c3ebe5e26273d68d3d68f7b2434030ca71a7091
Ivan-rom <rom_1van@icloud.com>
2024-10-01 21:11:38 +0600

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile, access, constants } from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  const folderPath = "./src/fs";
+  const fileContent = "I am fresh and young";
+
+  access(`${folderPath}/files/fresh.txt`, constants.F_OK, (err) => {
+    if (err) {
+      writeFile(`${folderPath}/files/fresh.txt`, fileContent, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
