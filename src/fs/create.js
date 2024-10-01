69c221af43ca3f2cd2c3d2b5bd41c3097ab81a50
Ivan-rom <rom_1van@icloud.com>
2024-10-01 22:54:13 +0600

    refactor: update fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,18 +2,19 @@ import { writeFile, access, constants } from "node:fs";
 
 const create = async () => {
   const folderPath = "./src/fs";
+  const fileDist = `${folderPath}/files/fresh.txt`;
   const fileContent = "I am fresh and young";
 
-  access(`${folderPath}/files/fresh.txt`, constants.F_OK, (err) => {
-    if (err) {
-      writeFile(`${folderPath}/files/fresh.txt`, fileContent, (error) => {
-        if (error) {
-          console.log(error);
-        }
-      });
-    } else {
+  access(fileDist, constants.F_OK, (err) => {
+    if (!err) {
       throw new Error("FS operation failed");
     }
+
+    writeFile(fileDist, fileContent, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
   });
 };
 
