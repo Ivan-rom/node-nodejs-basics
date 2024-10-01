c607a50af39a1b3c8119c109b8dadaa7bb2232f4
Ivan-rom <rom_1van@icloud.com>
2024-10-01 23:00:29 +0600

    feat: implement fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  const path = "./src/fs/files";
+  const source = `${path}/wrongFilename.txt`;
+  const dist = `${path}/properFilename.md`;
+
+  fs.access(source, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
+
+  fs.access(dist, fs.constants.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+
+    fs.rename(source, dist, (error) => {
+      if (error) console.log(error);
+    });
+  });
 };
 
 await rename();
