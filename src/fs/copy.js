66cfb8f837ff3e39d71cd0bf7102e93de4a138bf
Ivan-rom <rom_1van@icloud.com>
2024-10-01 22:47:02 +0600

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp, access, constants } from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  const folderPath = "./src/fs";
+  const source = `${folderPath}/files/`;
+  const dist = `${folderPath}/files_copy/`;
+
+  await access(dist, constants.F_OK, async (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+
+    await cp(source, dist, { recursive: true }, (error) => {
+      console.log(error);
+    });
+  });
 };
 
 await copy();
