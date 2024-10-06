3b10bfc03d9ee6ad412a654e4ffa47268706954d
Ivan-rom <rom_1van@icloud.com>
2024-10-06 20:11:39 +0600

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { EOL } from "os";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const stream = createReadStream(
+    "./src/hash/files/fileToCalculateHashFor.txt"
+  );
+
+  stream
+    .pipe(hash)
+    .setEncoding("hex")
+    .on("data", (data) => {
+      process.stdout.write(data + EOL);
+    });
 };
 
 await calculateHash();
