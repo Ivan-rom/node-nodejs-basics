86dd560d0117609301ee79993b6062bb136cabfa
Ivan-rom <rom_1van@icloud.com>
2024-10-06 20:27:21 +0600

    feat: implement zip/decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+import { createUnzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFile = "./src/zip/files/archive.gz";
+  const distFile = "./src/zip/files/fileToCompress.txt";
+
+  const unzip = createUnzip();
+  const readStream = fs.createReadStream(sourceFile);
+  const writeStream = fs.createWriteStream(distFile);
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
