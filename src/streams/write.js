c7b2b600df303289a90923b6788cb48009bd9ed9
Ivan-rom <rom_1van@icloud.com>
2024-10-06 11:58:58 +0600

    feat: implement streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+import { EOL } from "os";
+
 const write = async () => {
-    // Write your code here 
+  const path = "./src/streams/files/fileToWrite.txt";
+  const stream = fs.createWriteStream(path);
+
+  process.stdout.write("Type something to save it in fileToWrite.txt" + EOL);
+  process.stdout.write("To stop press CTRL + C" + EOL);
+
+  process.stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
 
 await write();
