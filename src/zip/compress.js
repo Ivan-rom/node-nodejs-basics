3660cafcdb339d78e443779b82db13f528f373f8
Ivan-rom <rom_1van@icloud.com>
2024-10-06 20:24:25 +0600

    feat: implement zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+import { createGzip } from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const sourceFile = "./src/zip/files/fileToCompress.txt";
+  const distPath = "./src/zip/files/archive.gz";
+
+  const gzip = createGzip();
+  const readStream = fs.createReadStream(sourceFile);
+  const writeStream = fs.createWriteStream(distPath);
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
