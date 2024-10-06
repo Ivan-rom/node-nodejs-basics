cf5a6eb038d0ab5a82662b7e755a732dec114b36
Ivan-rom <rom_1van@icloud.com>
2024-10-06 15:32:34 +0600

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { EOL } from "os";
+
 const transform = async () => {
-    // Write your code here 
+  process.stdout.write("Type something to reverse it in the console" + EOL);
+  process.stdout.write("To stop press CTRL + C" + EOL);
+
+  process.stdin.on("data", (data) => {
+    const reversedData = data.toString().split("").reverse().join("");
+
+    process.stdout.write(reversedData + EOL);
+  });
 };
 
 await transform();
