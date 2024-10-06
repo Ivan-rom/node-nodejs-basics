2895fcdd0b4c314eeadd613ff9b27d13c447c58b
Ivan-rom <rom_1van@icloud.com>
2024-10-06 09:28:52 +0600

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "node:fs";
+import { EOL } from "os";
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(
+    "./src/streams/files/fileToRead.txt",
+    "utf-8"
+  );
+
+  stream.on("readable", () => {
+    const data = stream.read();
+    process.stdout.write(data + EOL);
+  });
 };
 
 await read();
