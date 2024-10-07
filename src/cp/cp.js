58cf4e9707cca44675d33f15659936b2b0790661
Ivan-rom <rom_1van@icloud.com>
2024-10-07 18:08:49 +0600

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = "./src/cp/files/script.js";
+
+  const childProcess = spawn("node", [filePath, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
