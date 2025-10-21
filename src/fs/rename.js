3191be253730690bcf21e6193cc1df022f89488d
Ivan Romanenko <ivan_romanenko1@epam.com>
2025-10-21 13:58:35 +0600

    feat: complete fs/rename subtask

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from "node:fs";
+
+const DIRNAME = import.meta.dirname;
+const DIST_NAME = "properFilename.md";
+const SRC_NAME = "wrongFilename.txt";
+
 const rename = async () => {
-  // Write your code here
+  fs.access(`${DIRNAME}/files/${DIST_NAME}`, fs.constants.F_OK, (errDist) => {
+    // if DIST_NAME already exists
+    if (!errDist) throw new Error("FS operation failed");
+
+    fs.access(`${DIRNAME}/files/${SRC_NAME}`, fs.constants.F_OK, (errSrc) => {
+      // if SRC_NAME doesn't exist
+      if (errSrc) throw new Error("FS operation failed");
+
+      fs.rename(
+        `${DIRNAME}/files/${SRC_NAME}`,
+        `${DIRNAME}/files/${DIST_NAME}`,
+        (err) => {
+          // if something went wrong
+          if (err) throw new Error("FS operation failed");
+        }
+      );
+    });
+  });
 };
 
 await rename();
