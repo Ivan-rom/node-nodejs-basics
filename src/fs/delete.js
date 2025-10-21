c1eefd27875d39f03075b56f5b91a0a213b5f103
Ivan Romanenko <ivan_romanenko1@epam.com>
2025-10-21 14:02:57 +0600

    feat: complete fs/delete subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+
+const FILE_PATH = `${import.meta.dirname}/files/fileToRemove.txt`;
+
 const remove = async () => {
-  // Write your code here
+  fs.access(FILE_PATH, fs.constants.F_OK, (err) => {
+    if (err) throw new Error("FS operation failed");
+
+    fs.rm(FILE_PATH, (error) => {
+      if (error) throw new Error("FS operation failed");
+    });
+  });
 };
 
 await remove();
