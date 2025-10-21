6c36d313c700c155e20ea0dc4b33c697c31770b0
Ivan Romanenko <ivan_romanenko1@epam.com>
2025-10-21 15:12:19 +0600

    feat: complete cli/args subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-  // Write your code here
+  let res = "";
+
+  process.argv.slice(2).forEach((el, i) => {
+    if (i % 2 === 0) {
+      if (i !== 0) res += ", ";
+
+      res += `${el.slice(2)} is `;
+    } else {
+      res += `${el}`;
+    }
+  });
+
+  console.log(res);
 };
 
 parseArgs();
