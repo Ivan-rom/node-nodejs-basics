3281b641e25eddcf93ca551bd8c8eb2f83122a97
Ivan Romanenko <rom_1van@icloud.com>
2025-10-23 15:33:35 +0600

    feat: complete streams/write subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from "node:fs";
+
 const write = async () => {
-  // Write your code here
+  const writeStream = fs.createWriteStream(
+    `${import.meta.dirname}/files/fileToWrite.txt`
+  );
+
+  process.stdin.on("data", (chunk) => {
+    writeStream.write(chunk);
+  });
 };
 
 await write();
