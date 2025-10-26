f7a0ad9b36ed50d43895047cee48717ef52c72e5
Ivan Romanenko <rom_1van@icloud.com>
2025-10-26 11:58:13 +0600

    feat: complete streams/transform subtask

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-  // Write your code here
+  const stream = new Transform({
+    transform(chunk, _, callback) {
+      callback(null, chunk.toString().split("").reverse().join(""));
+    },
+  });
+
+  process.stdin.pipe(stream).pipe(process.stdout);
 };
 
 await transform();
