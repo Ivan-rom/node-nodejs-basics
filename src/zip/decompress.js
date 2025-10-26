6bd12361df3b87036b972eac7378f3345fbb1141
Ivan Romanenko <rom_1van@icloud.com>
2025-10-26 12:07:10 +0600

    feat: complete zlib/decompress subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import zlib from "node:zlib";
+import fs from "node:fs";
+
 const decompress = async () => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const srcPath = `${dirname}/files/archive.gz`;
+  const distPath = `${dirname}/files/fileToCompress.txt`;
+
+  const gunzip = zlib.createGunzip();
+  const readStream = fs.createReadStream(srcPath);
+  const writeStream = fs.createWriteStream(distPath);
+
+  readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
