61a482613cc3af30699ff9085672e623a515ee6b
Ivan-rom <rom_1van@icloud.com>
2025-10-26 20:06:47 +0600

    feat: complete wt subtask

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,14 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import worker_threads from "node:worker_threads";
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) throw new Error("Artificial error");
+
+  worker_threads.parentPort.postMessage(
+    nthFibonacci(worker_threads.workerData)
+  );
 };
 
 sendResult();
