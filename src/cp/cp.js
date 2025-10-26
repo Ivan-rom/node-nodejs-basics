255cec78fd009622fe228278a0d7da0407c83a93
Ivan-rom <rom_1van@icloud.com>
2025-10-26 21:30:24 +0600

    feat: complete cp subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-  // Write your code here
+import cp from "node:child_process";
+
+const DIRNAME = `${import.meta.dirname}/files/script.js`;
+
+const spawnChildProcess = async (args = []) => {
+  const childProcess = cp.spawn("node", [DIRNAME, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
